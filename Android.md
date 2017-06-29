48f511f7ed2cc8831190766cfd1d831ce35744b3
shenjianli <cqtddt@163.com>
2017-06-29 17:45:11 +0800

    commit the gradle

--- Android.md
+++
@@ -1,3 +0,0 @@
-
-
-[android动态修改应用图标和应用名称](http://www.jianshu.com/p/1d83f9a98ffd)
---
+++ interview/AndroidStudio中自定义Gradle插件/用AndroidStudio开发自定义 Gradle plugin - OPEN 开发经验库_files/z_stat.php
@@ -0,0 +1,43 @@
+			<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+			<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-CN" lang="zh-CN">
+				<head>
+					<title>Blocked URL: s95.cnzz.com</title>					<meta http-equiv="Pragma" content="no-cache" />
+					<meta http-equiv="Pragma-directive" content="no-cache" />
+					<meta http-equiv="cache-directive" content="no-cache" />
+					<link rel="stylesheet" href="styles/mi5.css" />
+										<script language="JavaScript">
+						<!--
+							function spywareinfo (signature) {
+								var details = window.open("spywareinfo.php?sid=" + signature,'detail','width=750,height=400,scrollbars=1,status=no,resizable');
+								details.focus();
+							}
+						//-->
+					</script>
+				</head>
+				<body>
+				<div id="mainContent">
+	<div id="mainText">
+				<img src="images/upload/ICBC.jpg" alt="Symantec Defense Centre" />
+		<hr noshade="noshade" size="1" style="margin-bottom: 10px;" />
+		<table>
+			<tr>
+								<td style="padding-left: 15px;">
+					<h3>Symantec Web Gateway</h3>
+					<h1>正在访问网站: s95.cnzz.com 当前被禁止</h1>									<p>
+										URL: s95.cnzz.com/z_stat.php?id=1257892335									</p>
+								<p class='translated'>此 URL，s95.cnzz.com，是一个已知的 恶意软件域 位置并且违反公司策略。</p>					<p class='translated'>
+						<a href="javascript:spywareinfo('805306452');">单击此处 </a>
+						以获取更多信息。					</p>
+														<p class='translated'>如果您认为此检测存在错误，请										<a href="blocked.php?ip=10.10.21.122&profile=3&detection=10&url=s95.cnzz.com&id=805306452&history=-2&u=s95.cnzz.com%2Fz_stat.php%3Fid%3D1257892335"
+											onclick="alert('已将您的建议提交给 IT 部门。谢谢。')">
+											单击此处。										</a>
+									</p>
+												</td>
+			</tr>
+		</table>
+	</div>
+	<div class="copyright">&copy; Copyright 2004-2017, Symantec Corporation. All rights reserved.</div>
+</div>
+				</body>
+			</html>
+		
